78dab6bd7093bdce91054c45b469a5a08ea2378d
issa097 <haddadissa962@gmail.com>
2023-12-17 10:09:27 +0300

    responsive dashboard

--- Dashboard/client/src/Pages/UserTable.jsx
+++ Dashboard/client/src/Pages/UserTable.jsx
@@ -14,6 +14,9 @@ function UserTable() {
   const [limit, setlimit] = useState(0);
   console.log(limit);
   const [selectedUserId, setSelectedUserId] = useState(null);
+  const tableStyles = {
+    overflowX: "auto",
+  };
   console.log("hhhhhhhhhhhh", users);
   useEffect(() => {
     const fetchData = async () => {
@@ -99,7 +102,7 @@ function UserTable() {
     <>
       <Statics />
 
-      <div className="overflow-hidden rounded-lg border border-[#C08261] shadow-md m-5 ml-96 mt-30">
+      <div className="overflow-hidden rounded-lg border border-[#C08261] shadow-md m-5  mt-30">
         <div className="flex items-center rounded-none border-none    space-x-2">
           <input
             type="text"
@@ -117,87 +120,89 @@ function UserTable() {
           </button>
         </div>
         <br />
-        <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
-          <thead className="bg-[#C08261]">
-            <tr>
-              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                State
-              </th>
-              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                Role
-              </th>
-              <th scope="col" className="px-6 py-4 font-medium text-gray-900">
-                Delete
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-            {(searchResults.length > 0 ? searchResults : users || []).map(
-              (user, index) => (
-                <tr
-                  key={user.user_id}
-                  className={`hover:bg-gray-50 ${
-                    index % 2 !== 0 ? "bg-white" : "bg-[#F7F1EE]"
-                  }`}
-                >
-                  <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                    <div className="relative h-10 w-10">
-                      <img
-                        className="h-full w-full rounded-full object-cover object-center"
-                        src={user.user_img}
-                        alt=""
-                      />
-                      <span
-                        className={`absolute right-0 bottom-0 h-2 w-2 rounded-full bg-${
-                          user.active ? "green" : "red"
-                        }-400 ring ring-white`}
-                      ></span>
-                      {/* <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span> */}
-                    </div>
-                    <div className="text-sm">
-                      <div className="font-medium text-gray-700">
-                        {user.username}
+        <div className="table-container" style={tableStyles}>
+          <table className="w-full border-collapse bg-white text-left text-sm text-gray-500">
+            <thead className="bg-[#C08261]">
+              <tr>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  State
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Role
+                </th>
+                <th scope="col" className="px-6 py-4 font-medium text-gray-900">
+                  Delete
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+              {(searchResults.length > 0 ? searchResults : users || []).map(
+                (user, index) => (
+                  <tr
+                    key={user.user_id}
+                    className={`hover:bg-gray-50 ${
+                      index % 2 !== 0 ? "bg-white" : "bg-[#F7F1EE]"
+                    }`}
+                  >
+                    <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
+                      <div className="relative h-10 w-10">
+                        <img
+                          className="h-full w-full rounded-full object-cover object-center"
+                          src={user.user_img}
+                          alt=""
+                        />
+                        <span
+                          className={`absolute right-0 bottom-0 h-2 w-2 rounded-full bg-${
+                            user.active ? "green" : "red"
+                          }-400 ring ring-white`}
+                        ></span>
+                        {/* <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span> */}
                       </div>
-                      <div className="text-gray-400">{user.email}</div>
-                    </div>
-                  </th>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`inline-flex items-center gap-1 rounded-full ${
-                        user.active ? "bg-green-50" : "bg-red-50"
-                      } px-2 py-1 text-xs font-semibold text-${
-                        user.active ? "green" : "red"
-                      }-600`}
-                    >
+                      <div className="text-sm">
+                        <div className="font-medium text-gray-700">
+                          {user.username}
+                        </div>
+                        <div className="text-gray-400">{user.email}</div>
+                      </div>
+                    </th>
+                    <td className="px-6 py-4">
                       <span
-                        className={`h-1.5 w-1.5 rounded-full ${
-                          user.active ? "bg-green-600" : "bg-red-600"
-                        }`}
-                      ></span>
-                      {user.active ? "Active" : "Inactive"}
-                    </span>
-                  </td>
-
-                  <td className="px-6 py-4">{user.role}</td>
-                  {/* <td className="px-6 py-4"></td> */}
-                  <td className="px-6 py-4 ">
-                    <div className="flex justify-start gap-4">
-                      <button
-                        className="px-4 py-2 bg-[#C08261] text-white rounded-lg"
-                        onClick={() => handleDelete(user.user_id)}
+                        className={`inline-flex items-center gap-1 rounded-full ${
+                          user.active ? "bg-green-50" : "bg-red-50"
+                        } px-2 py-1 text-xs font-semibold text-${
+                          user.active ? "green" : "red"
+                        }-600`}
                       >
-                        {user.is_deleted ? "Unblock" : "Block"}
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </table>
+                        <span
+                          className={`h-1.5 w-1.5 rounded-full ${
+                            user.active ? "bg-green-600" : "bg-red-600"
+                          }`}
+                        ></span>
+                        {user.active ? "Active" : "Inactive"}
+                      </span>
+                    </td>
+
+                    <td className="px-6 py-4">{user.role}</td>
+                    {/* <td className="px-6 py-4"></td> */}
+                    <td className="px-6 py-4 ">
+                      <div className="flex justify-start gap-4">
+                        <button
+                          className="px-4 py-2 bg-[#C08261] text-white rounded-lg"
+                          onClick={() => handleDelete(user.user_id)}
+                        >
+                          {user.is_deleted ? "Unblock" : "Block"}
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
         <div className="flex justify-center mt-4">
           {Array.from({ length: totalPages }, (_, index) => (
             <button
